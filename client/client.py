d584bc097b601733d9b7382b17c50c06076b9edb
Jason Morley <hello@jbmorley.co.uk>
2024-03-07 19:05:52 -1000

    fix: Persist the last anchor in the client (#4)
    This ensures the client will not attempt to re-download all items on
    next restart.

--- client/client.py
+++ client/client.py
@@ -1,42 +1,70 @@
 import argparse
+import os
 import time
 
 import requests
+import yaml
 
-SERVICE_URL = "http://0.0.0.0:9449"
+SETTINGS_PATH = os.path.expanduser("~/.config/writeme/settings.yaml")
 
+class Client:
 
-def get_queue(last_anchor=-1):
-    response = requests.get(SERVICE_URL + "/api/v1/queue",
-                            params={
-                                "last_anchor": last_anchor,
-                                "page_size": 5,
-                            })
-    queue = response.json()
-    return queue
+    def __init__(self, url):
+        self.url = url
 
+    def get_queue(self, last_anchor=-1):
+        response = requests.get(self.url + "/api/v1/queue",
+                                params={
+                                    "last_anchor": last_anchor,
+                                    "page_size": 5,
+                                })
+        queue = response.json()
+        return queue
 
-def download_item(identifier):
-    response = requests.get(SERVICE_URL + "/api/v1/queue/" + identifier,
-                            stream=True)
-    with open(f"{identifier}.png", 'wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
+    def download_item(self, identifier):
+        response = requests.get(self.url + "/api/v1/queue/" + identifier,
+                                stream=True)
+        path = f"{identifier}.png"
+        if os.path.exists(path):
+            return path
+        with open(path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+        return path
 
 
 def main():
     parser = argparse.ArgumentParser()
     options = parser.parse_args()
 
-    last_anchor = -1
+    # Load the settings.
+    with open(SETTINGS_PATH) as fh:
+        settings = yaml.safe_load(fh)
+
+    # Read the settings with defaults.
+    destination = os.path.expanduser(settings["destination"])
+    url = settings["url"] if "url" in settings else "https://writeme-staging.jbmorley.co.uk"
+    last_anchor = settings["last_anchor"] if "last_anchor" in settings else -1
+
+    # Change to the destination directory.
+    os.chdir(destination)
+
+    # Poll the queue.
+    client = Client(url=url)
     while True:
         try:
-            queue = get_queue(last_anchor=last_anchor)
+            queue = client.get_queue(last_anchor=last_anchor)
             for item in queue:
                 print(item["anchor"], item["identifier"])
-                download_item(item["identifier"])
+                client.download_item(item["identifier"])
                 last_anchor = max(last_anchor, item["anchor"])
+
+                # Save the last anchor.
+                settings["last_anchor"] = last_anchor
+                with open(SETTINGS_PATH, "w") as fh:
+                    yaml.dump(settings, fh)
+
         except requests.exceptions.ConnectionError:
             pass
         time.sleep(10)
